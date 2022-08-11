f1f4636a3287d21853d1643413cd54fd2eb67941
dilumsadeepa <51749100+dilumsadeepa@users.noreply.github.com>
2022-08-11 15:55:34 +0530

    updated

--- project/vite.config.js
+++ project/vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/dilumsadeepa.github.io/',
+  base: '',
   plugins: [vue()],
   resolve: {
     alias: {
